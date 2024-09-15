b5899e72e722ba6ff751863f4d61aec51c175529
dansheng061 <73804513+StupidBoy558@users.noreply.github.com>
2024-09-15 12:10:12 +0800

    脚手架构建

--- web/web-admin/src/main/java/com/atguigu/Main8080.java
+++ web/web-admin/src/main/java/com/atguigu/Main8080.java
@@ -1,10 +1,12 @@
 package com.atguigu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan("com.atguigu.mapper")
 public class Main8080 {
 
     public static void main(String[] args) {
