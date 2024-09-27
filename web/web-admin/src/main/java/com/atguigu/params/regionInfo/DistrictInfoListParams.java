0a997a0e0a191a26a015b9314ba3c7c52b1efd25
dansheng <73804513+StupidBoy558@users.noreply.github.com>
2024-09-27 22:15:13 +0800

    完成区域信息管理接口

--- web/web-admin/src/main/java/com/atguigu/params/regionInfo/DistrictInfoListParams.java
+++ web/web-admin/src/main/java/com/atguigu/params/regionInfo/DistrictInfoListParams.java
@@ -3,12 +3,14 @@ package com.atguigu.params.regionInfo;
 import com.atguigu.params.BaseParams;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @Description: 城市信息列表参数
  * @Author: dansheng
  * @CreateTime: 2024/9/24
  **/
+@EqualsAndHashCode(callSuper = false)
 @Data
 @Schema(name = "DistrictInfoListParams", description = "区县信息列表参数")
 public class DistrictInfoListParams extends BaseParams {
